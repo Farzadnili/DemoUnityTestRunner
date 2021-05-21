3d2db8a5acebc9e75e4e00621f31f7dbb1f00091
amirfarzadnilizadeh <a.farzadnilizadeh@gmail.com>
2021-05-21 15:55:07 +0430

    Fix Jump(for later)

--- Demo Unity Test Runner/Assets/Scripts/Jump.cs
+++ Demo Unity Test Runner/Assets/Scripts/Jump.cs
@@ -6,6 +6,7 @@ public class Jump : MonoBehaviour
 {
     Rigidbody rb;
     public float JumpHigh;
+    public Terrain terrain;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,15 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("j")){
+      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Terrain" && Input.GetKeyDown("j")){
             Vector3 jump = new Vector3(0,JumpHigh,0); 
             rb.AddForce(jump);
-        }   
+        }
     }
+
 }
